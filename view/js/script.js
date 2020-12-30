dedd5382d9150f26795d22dbc77f721532be8844
charcoudal <61440085+charcoudal@users.noreply.github.com>
2020-12-30 17:51:44 -0500

    Update script.js

--- view/js/script.js
+++ view/js/script.js
@@ -11,3 +11,65 @@ function allLetter(inputtxt)
       return false;
       }
       }
+//function to run when button is clicked
+let clickFunc = () => {
+  //show loader icon
+  document.getElementsByClassName("loader")[0].style.display = 'revert'
+
+  //get input
+  let inp = document.getElementById("input").value
+  console.log("input: ", inp)
+
+  //if incorrect input then display alert and end function
+  if (!inp.match("^[A-Za-z]+$")) {
+    alert('Please provide the valid input')
+    //hide loader icon
+    document.getElementsByClassName("loader")[0].style.display = 'none'
+    return;
+  }
+
+  //add input to url api call
+  let url = `https://jsonmock.hackerrank.com/api/cities/?city=${inp}`
+  //https://jsonmock.hackerrank.com/api/cities/?city=a
+
+  //make fetch get request
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+
+      //hide loader icon
+      document.getElementsByClassName("loader")[0].style.display = 'none'
+
+      //show total # of cities
+      document.getElementById("totalCount").innerText = `Total cities found: ${data.data.length}`
+
+      //create tbody on table
+      let tbody = document.createElement("tbody")
+      document.getElementsByClassName("table")[0].appendChild(tbody)
+
+      //add table row and create variable to check when state changes
+      let tr
+      let state
+      //loop through all items and add cities to rows
+      for(const item of data.data){
+        //if new state then add new row
+        if(item.state != state){
+          state = item.state
+
+          tr = tbody.insertRow()
+          let td = document.createElement("td")
+          td.innerText = state
+          tr.appendChild(td)
+        }
+
+        //add cities to current row
+        let td = document.createElement("td")
+        td.innerText = item.city
+        tr.appendChild(td)
+      }
+    });
+}
+
+//add click event to button
+document.getElementById("button").addEventListener("click", clickFunc)
